49b9cbbd65964a5a0b32bd32179955176979e67a
Nitin Chaurasia <nitin.chaurasia@davita.com>
2020-10-01 00:52:06 -0500

    Update TransactionGroup.java

--- src/main/java/com/nitin/transactions/runner/TransactionGroup.java
+++ src/main/java/com/nitin/transactions/runner/TransactionGroup.java
@@ -3,7 +3,10 @@ package com.nitin.transactions.runner;
 import com.nitin.transactions.ReadTransactionsCsv;
 import com.nitin.transactions.entity.Transaction;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -11,7 +14,11 @@ import java.util.stream.Collectors;
  * @create on Wednesday, September/30/2020 at 10:26 PM
  */
 
+//https://www.oracle.com/technical-resources/articles/java/architect-streams-pt2.html
 public class TransactionGroup {
+    private static final Comparator<Map.Entry<String, Double>> valueOrder = Map.Entry.comparingByValue();
+    private static final Comparator<Map.Entry<String, Double>> reversedValueOrder = valueOrder.reversed();
+
     public static void main(String[] args) {
         List<Transaction> transactions = ReadTransactionsCsv.getData();
 
@@ -38,5 +45,25 @@ public class TransactionGroup {
                 .collect(Collectors.groupingBy(Transaction::getType,
                         Collectors.summingDouble(Transaction::getValue)))
                 .entrySet().forEach(System.out::println);
+
+        System.out.println("**************************************************************");
+        System.out.println("Get max amount by each City");
+
+        getMaxAmountByCity(transactions);
+    }
+
+    private static void getMaxAmountByCity(List<Transaction> transactions) {
+        transactions.stream()
+               // .collect(Collectors.groupingBy(Transaction::getCity,
+               //         Collectors.maxBy(Comparator.comparing(Transaction::getValue))))
+                .collect(Collectors.groupingBy(Transaction::getCity,
+                            Collectors.collectingAndThen
+                                    (Collectors.maxBy(Comparator.comparing(Transaction::getValue)),
+                                                            optional -> optional.get().getValue())))
+                .entrySet()
+                .stream()
+                .sorted(reversedValueOrder)
+                .limit(10)
+                .forEach(l -> System.out.printf("%20s : %5f\n", l.getKey(), l.getValue()));
     }
 }
